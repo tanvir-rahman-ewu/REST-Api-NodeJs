c7fef804a2e32aecd32eda7a8c596b3e540003a2
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-12 20:33:22 +0600

    get put delete post implemented

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
-const mongoClient = require('mongodb').mongoClient
+const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
 const note_routes = require('./app/routes/note_routes')
-
+const db = require('./config/db')
 const app = express()
 const port = 3000
 
 app.use(bodyParser.urlencoded({extended : true}))
 
-const index = require('./app/routes/index')
-index(app, {})
 
 
-app.listen(port, ()=>{
-    console.log(`we are live at localhost:${port}`)
+
+MongoClient.connect(db.url, (err, client) =>{
+    if(err)
+    {
+        return console.log(err)
+    }
+    let database = client.db('ChelseaDB');
+
+    require('./app/routes')(app, database)
+    
+    app.listen(port, ()=>{
+        console.log(`we are live at localhost:${port}`)
+    })
 })
